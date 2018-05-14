b1b8c424e7ec05b64846c3c36f6e4cc5db27335e
simxn <171wita27@o365.htl-leoben.at>
2018-05-14 14:16:51 +0200

    improved Minesweeper

--- Game.java
+++ Game.java
@@ -3,17 +3,19 @@ public class Game {
 	public static void main(String[] args) {
 		
 		String[][] field = setField(3, 3);
-		field = setMine(0, 3);
+		//field = setMine(0, 3);
+		showField(field);
 	}
 
 	public static String[][] setField(int rows, int collums) {
-
-		String[][] myField = new String[rows][collums];
-		return myField;
-	}
-	public static String[][] setMine(int rows, int collums) {
+		String[][] mineField = new String[rows][collums];
+		for(int i = 0; i<rows; i++) {
+			for(int j = 0; j < collums; j++) {
+				mineField[i][j] = "[ ]";
+			}
+		}
 		
-		String[][] mine = new String[rows][collums];
-		return mine;		
+		return mineField;
 	}
+	public static 
 }
